039e637efbfd0b22e36a3c1eb1802b5f8ed5fba5
Aleksandar Kiprovski <ak4966@cuda2.cims.nyu.edu>
2017-10-22 22:10:49 -0400

    updating testing alexnet jupyter notebook

--- pytorch_train.py
+++ pytorch_train.py
@@ -15,7 +15,7 @@ import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 import torch.optim
 import torch.utils.data
-import torch.utils.data.distributed
+#import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import models as models
